f0cbc5af77c0c92902e63fb6840f8b7ca586687b
jroyals2 <jamesroyals3@gmail.com>
2017-09-29 13:57:13 -0400

    finished the routes for the beer.

--- routes/beer.js
+++ routes/beer.js
@@ -31,15 +31,108 @@ router.get('/new', (request, response) => {
 
 
 // CREATE
+router.post('/', (request, response) => {
+    const breweryId = request.params.breweryId
+    const newBeer = request.body
+    BreweryModel.findById(breweryId)
+        .then((brewery) => {
+            brewery.beers.push(newBeer)
+            return brewery.save()
+        })
+        .then(() => {
+            response.redirect(`/breweries/${breweryId}/beers`)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
 
 // EDIT
+    router.get('/:beerId/edit', (request, response) => {
+        const breweryId = request.params.breweryId
+        const beerId = request.params.beerId
 
+        BreweryModel.findById(breweryId)
+            .then((brewery) => {
+                const beer = brewery.beers.id(beerId)
+                response.render('beer/edit', {
+                    beer: beer,
+                    breweryId: breweryId
+                })
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    })
 // UPDATE
+
+router.put('/:beerId', (request, response) => {
+    const breweryId = request.params.breweryId
+    const beerId = request.params.beerId
+    const updatedBeer = request.body
+
+    BreweryModel.findByIdAndUpdate(breweryId)
+        .then((brewery) => {
+            const beer = brewery.beers.id(beerId)
+
+            beer.name = updatedBeer.name
+            beer.abv = updatedBeer.abv
+            beer.ibus = updatedBeer.ibus
+            beer.description = updatedBeer.description
+            beer.tastingNotes = updatedBeer.tastingNotes
+
+            return brewery.save()
+        })
+        
+        .then(() => {
+            response.redirect(`/breweries/${breweryId}/beers/${beerId}`)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    
+
+})
 
 // SHOW 
 
+    router.get('/:beerId', (request, response) => {
+        const breweryId = request.params.breweryId
+        const beerId = request.params.beerId
+
+        BreweryModel.findById(breweryId)
+            .then((brewery) => {
+                const beer = brewery.beers.id(beerId)
+                response.render('beer/show', {
+                    beer: beer,
+                    breweryId: breweryId,
+                    brewery: brewery
+                })
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
+    })
+
 // DELETE
 
+router.get('/:beerId/delete', (request, response) => {
+    const breweryId = request.params.breweryId
+    const beerId = request.params.beerId
+
+    BreweryModel.findById(breweryId)
+     .then((brewery) => {
+         const beer = brewery.beers.id(beerId).remove()
+         return brewery.save()
+     })
+     .then(() => {
+         response.redirect(`/breweries/${breweryId}/beers`)
+     })
+     .catch((error) => {
+         console.log(error)
+     })
+})
 
 
 
